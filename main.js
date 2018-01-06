59b9f325844e570a27dab60dd0d662fcfa820d3d
Tim Rodenbröker <post@timrodenbroeker.de>
2018-01-06 14:20:34 +0100

    a

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+
+
+
 function randomInt(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -7,22 +11,17 @@ function mapRange(value, low1, high1, low2, high2) {
         return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
     }
 
-    function getScrollPercent() {
-        var h = document.documentElement, 
-            b = document.body,
-            st = 'scrollTop',
-            sh = 'scrollHeight';
-        return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
-    }
 
+
+var arrWithoutShortSentences = [];
 
 
 $(document).ready(function(){
-	function pullStuff(keyword, target){
+    function pullStuff(keyword, target){
  
     $.ajax({
         type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+keyword+"&callback=?",
+        url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+keyword+"&callback=?",
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
@@ -30,13 +29,13 @@ $(document).ready(function(){
  
             var markup = data.parse.text["*"];
 
-			function strip(html){
-				var tmp = document.createElement("DIV");
-				tmp.innerHTML = html;
-				return tmp.textContent || tmp.innerText || "";
-			}
+            function strip(html){
+                var tmp = document.createElement("DIV");
+                tmp.innerHTML = html;
+                return tmp.textContent || tmp.innerText || "";
+            }
 
-			var cleanmarkup = strip(markup)
+            var cleanmarkup = strip(markup)
                 .replace(/(\r\n|\n|\r)/gm," ")
                 .replace(/\s\s+/g, ' ')
                 .replace(/([.?!])\s*(?=[A-Z])/g, "$1|")
@@ -45,23 +44,23 @@ $(document).ready(function(){
 
             var arr = cleanmarkup.split("|");
 
-            var arrWithoutShortSentences = [];
+            arrWithoutShortSentences = [];
 
             for(var i = 0; i < arr.length; i++){
 
-                if (arr[i].length > 10){
+                if (arr[i].length > 100){
                     arrWithoutShortSentences.push(arr[i]);
                 }
 
             }
 
-            console.log(arrWithoutShortSentences, arrWithoutShortSentences.length);
+            // console.log(arrWithoutShortSentences, arrWithoutShortSentences.length);
 
             var randomnt = Math.floor(randomInt(0,arrWithoutShortSentences.length));
 
             var sentence = arrWithoutShortSentences[randomnt];
 
-        $(target).append(sentence);			
+        $(target).append(sentence);         
             
         },
         error: function (errorMessage) {
@@ -71,40 +70,48 @@ $(document).ready(function(){
     
     
     }
-		pullStuff("Laozi","#target"); 
-        pullStuff("Laozi","#back"); 
+        pullStuff("Kinshasa","#target"); 
+
   
 });
 
 
+function getScrollPercent() {
+    var h = document.documentElement, 
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight';
+    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+}
+
+var trigger = 0;
 
 
-    
-    
+function rotator(rotations){
 
-
-
-$(window).scroll(function() {
-
-
-        var rotation = mapRange(getScrollPercent(), 0,100,0,720);
+        var rotation = mapRange(getScrollPercent(), 0,100,0,rotations*360)+90+180;
         var scaling = mapRange(getScrollPercent(), 0,100,1,1);
-        var trigger = Math.floor(mapRange(getScrollPercent(), 0,100,0,720/180));
 
-        console.log(trigger);
+        trigger = Math.floor(mapRange(rotation, 0,rotations*360,0,(rotations)*2));
 
-    if (trigger % 1 ==0){
+        // var style = 'scale(' + scaling + ') rotateY(' + (rotation + 90) + 'deg) rotateX(' + (rotation + 90) + 'deg)';
+        var style = 'scale(' + scaling + ') rotateY(' + (rotation + 90) + 'deg)';
 
-    }
+        $('#plate').css({
+            '-webkit-transform' : style,
+            '-moz-transform'    : style,
+            '-ms-transform'     : style,
+            '-o-transform'      : style,
+            'transform'         : style
+        });
 
-            $('#plate').css({
-                '-webkit-transform' : 'scale(' + scaling + ') rotateY(' + rotation + 'deg)',
-                '-moz-transform'    : 'scale(' + scaling + ') rotateY(' + rotation + 'deg)',
-                '-ms-transform'     : 'scale(' + scaling + ') rotateY(' + rotation + 'deg)',
-                '-o-transform'      : 'scale(' + scaling + ') rotateY(' + rotation + 'deg)',
-                'transform'         : 'scale(' + scaling + ') rotateY(' + rotation + 'deg)'
-            });
-        
+        $('#plate').html(arrWithoutShortSentences[trigger]);
+}
 
-        
+
+
+
+    $(window).scroll(function() {
+        rotator(4);
     });
+
